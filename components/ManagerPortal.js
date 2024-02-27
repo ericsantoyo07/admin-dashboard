271d4c6ad762c85a472b4327a5c6a10ebc07ad4d
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-02-27 21:54:55 +0600

    added email to owner portal

--- components/ManagerPortal.js
+++ components/ManagerPortal.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { supabase } from "@/database/supabase";
+import { LogOut } from 'lucide-react';
+import styles from '@/styles/OwnerPortal.module.css';
+import Articles from './Articles';
+import { useAuth } from '@/hooks/useAuth';
 
 const ManagerPortal = () => {
+
+  const { role, session } = useAuth();
   return (
-      <div>
-          <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
+    <div className={styles.owner_portal}>
+      <div className={styles.nav_bar}>
+        <div>
+          <h1>Manager Portal</h1>
+        </div>
+        {/* <button >Sign Out</button> */}
+        <div className={styles.user_info}>
+
+          <p> {session?.user?.email}</p>
+          <LogOut size={30} onClick={() => supabase.auth.signOut()} />
+        </div>
       </div>
+      <Articles />
+    </div>
   )
 }
 
