b4637daaa49fd13b927a9ecfd7620b56c7f42597
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-02-28 21:47:51 +0600

    updated the guest portal

--- components/GuestPortal.js
+++ components/GuestPortal.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { supabase } from "@/database/supabase";
+import { Page } from './Page';
 
 const GuestPortal = () => {
   return (
-      <div>GuestPortal
-          <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
-    </div>
+    <Page>
+      <h3>
+        Welcome to the Guest Portal
+      </h3>
+      <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
+    </Page>
   )
 }
 
